c69593f5b3a52616f6da3c74b4e200e379aade4a
AB7zz <abhinavcv007@gmail.com>
2023-10-03 18:54:57 +0530

    changes

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -6,6 +6,9 @@ import threefournine from '../assets/349.jpeg'
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore, collection, addDoc } from "firebase/firestore";
+import emailjs from '@emailjs/browser';
+
+emailjs.init('user_MzVXLN7rOfB1swxEYSxO0')
 
 const firebaseConfig = {
   apiKey:  import.meta.env.VITE_apiKey,
@@ -114,11 +117,51 @@ const Register = () => {
         try {
             console.log(formData)
             localStorage.setItem('name', formData.name)
-            const docRef = await addDoc(collection(db, "registeredUsers"), {
-              formData: formData,
-            });
-            console.log("Document written with ID: ", docRef.id);
-            window.location.replace('/booked')
+            if(formData.file != ''){
+              setError(null)
+              const docRef = await addDoc(collection(db, "registeredUsers"), {
+                formData: formData,
+              });
+              // await emailjs.send("service_6p7k38d","template_s87nuyi",{
+              //   message: `Hey ${formData.name}!, we are delighted to confirm your registration for the SpaceUp CUSAT event scheduled for October 14th, from 8:00 AM to 7:00 PM at CUSAT.
+              //   \n\n
+              //   Please keep the following event details in mind:
+              //   \n\n
+              //   Event Date: October 14, 2023\n
+              //   Event Time: 8:00 AM - 7:00 PM\n
+              //   Venue: CUSAT, kochi\n
+              //   \n\n
+              //   You will need to present the following code at the event venue for entry:
+              //   \n\n
+              //   Your Event Code: ${formData.code}
+              //   \n\n
+              //   Please make sure to have this code readily available on your mobile device or in print form to streamline your registration process.
+              //   \n\n
+              //   Should you have any inquiries or require further assistance, do not hesitate to contact us at.
+              //   \n\n
+              //   Fimil:+91 9544664678\n
+              //   Yadu:+91 9895223267\n
+              //   \n\n
+              //   We appreciate your participation and look forward to sharing an exciting and informative day with you at SpaceUp CUSAT!
+              //   \n\n
+              //   Best regards,
+              //   \n\n
+              //   SpaceUP Team`,
+              //   toEmail: formData.email,
+              //   })
+              //   .then((result) => {
+              //     console.log(result);
+              //     // setEmail("");
+              //     // setMessage("");
+              //     // alert('Claim email sent succesfully')
+              //   }, (error) => {
+              //     console.log(error);
+              //   });
+              console.log("Document written with ID: ", docRef.id);
+              window.location.replace('/booked')
+            }else{
+              setError('Please attach the screenshot of the payment')
+            }
           } catch (error) {
             console.error("Error adding document: ", error);
           }
@@ -175,6 +218,7 @@ const Register = () => {
                     <div className='flex my-5'>
                         <button onClick={() => setNext(false)} className='px-7 py-3 border-[1.5px] border-black mr-3 border-[1.5px] border-black'>Back</button>
                         <button onClick={handleSubmit} className='px-7 py-3 border-[1.5px] border-black bg-[#E51F24] text-white border-none'>Submit</button>
+                        {error && <p className='text-red-500 text-xl my-5 font-bold text-center'>{error}</p>}
                     </div>
                 </div>}
             </div>    
